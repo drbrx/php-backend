88c6522e238067c35a453a42c831ac7c0dad0d6c
Joel bosio <bosio.joel@volta.ts.it>
2022-11-18 14:00:18 +0100

    updates

--- src/main/main.php
+++ src/main/main.php
@@ -7,13 +7,37 @@ $table = 'tabella';
 $stmt = $pdo->prepare("SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='" . $table . "'");
 $stmt->execute();
 $stmtResponse = $stmt->fetchAll();
+?>
 
-foreach ($stmtResponse as $currentRecord) {
-    if ($currentRecord["TABLE_SCHEMA"] == $_SESSION['db_name']) {
-        echo var_dump($currentRecord) . "<br></br>";
-        echo $currentRecord["COLUMN_NAME"] . " > " . $currentRecord["COLUMN_TYPE"] . "<br></br>";
-        if(substr($currentRecord["TABLE_SCHEMA"], 0, 2) == "id"){
-            echo "foreign";
+<form>
+    <table>
+
+        <?php
+        foreach ($stmtResponse as $currentRecord) {
+            if ($currentRecord["TABLE_SCHEMA"] == $_SESSION['db_name']) {
+                //echo var_dump($currentRecord);
+                echo "<td>" . $currentRecord["COLUMN_NAME"] . "</td><td>";
+                $maxLenght = preg_replace("/[^0-9]/", "", $currentRecord["COLUMN_TYPE"]);
+                $dataType = strtok($currentRecord["COLUMN_TYPE"], '(');
+                switch ($dataType) {
+                    case "int":
+                        echo "<input type=\"text\" pattern=\"\d*\" maxlength=\"" . $maxLenght . "\" name=\"" . $currentRecord["COLUMN_NAME"] . "\" id=\"" . $currentRecord["COLUMN_NAME"] . "\" placeholder=\"numero di max " . $maxLenght . " cifre\"></input>";
+                        break;
+                    case "varchar":
+                        echo $maxLenght > 20 ? "<textarea " : "<input type=\"text\"".
+                        " \" maxlength=\"" . $maxLenght . "\" name=\"" . $currentRecord["COLUMN_NAME"] . "\" id=\"" . $currentRecord["COLUMN_NAME"] . "\" value=\"\" placeholder=\"testo di max " . $maxLenght . " caratteri\"></input>";
+                        break;
+                    case "date":
+                        echo "<input type=\"date\" name=\"" . $currentRecord["COLUMN_NAME"] . "\" id=\"" . $currentRecord["COLUMN_NAME"] . "\"></input>";
+                        break;
+                }
+                echo "</td></tr>";
+            }
         }
-    }
-}
+        ?>
+        <tr>
+            <td>Submit</td>
+            <td><input type="submit" name="submit" value="Invia" /></td>
+        </tr>
+    </table>
+</form>
