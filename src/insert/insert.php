70617fc053fa47191f65e54a468fa7d14c97250e
Joel bosio <bosio.joel@volta.ts.it>
2022-11-24 11:39:29 +0100

    structured insertion

---
+++ src/insert/insert.php
@@ -0,0 +1,57 @@
+<?php
+require_once("../common/php/DBConnector.php");
+$config = fopen("../../config.cfg", "r");
+if ($config != false) {
+    $configInfo = array();
+    while (!feof($config)) {
+        $lineResult = array();
+        $lineResult = explode(": ", fgets($config));
+        $configInfo += [$lineResult[0] => preg_replace('/[^a-zA-Z0-9_ %\[\]\.\(\)%&-]/s', '', $lineResult[1])];
+    }
+    echo var_dump($configInfo);
+    fclose($config);
+}
+
+$connMySQL = new ConnectionMySQL();
+$pdo = $connMySQL->getConnection();
+$table = 'tabella';
+$stmt = $pdo->prepare("SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='" . $table . "'");
+$stmt->execute();
+$stmtResponse = $stmt->fetchAll();
+?>
+
+<form>
+    <table>
+
+        <?php
+        foreach ($stmtResponse as $currentRecord) {
+            if ($currentRecord["TABLE_SCHEMA"] == $_SESSION['db_name']) {
+                //echo var_dump($currentRecord);
+                echo "<td>" . $currentRecord["COLUMN_NAME"] . "</td><td>";
+                $maxLenght = preg_replace("/[^0-9]/", "", $currentRecord["COLUMN_TYPE"]);
+                $dataType = strtok($currentRecord["COLUMN_TYPE"], '(');
+                switch (isset($configInfo[$currentRecord["COLUMN_NAME"]]) ? $configInfo[$currentRecord["COLUMN_NAME"]] : $dataType) {
+                    case "int":
+                        echo "<input type=\"text\" pattern=\"\d*\" maxlength=\"" . $maxLenght . "\" name=\"" . $currentRecord["COLUMN_NAME"] . "\" id=\"" . $currentRecord["COLUMN_NAME"] . "\" placeholder=\"numero di max " . $maxLenght . " cifre\"></input>";
+                        break;
+                    case "varchar":
+                        echo ($maxLenght > 20 ? "<textarea" : "<input type=\"text\"") .
+                            " \" maxlength=\"" . $maxLenght . "\" name=\"" . $currentRecord["COLUMN_NAME"] . "\" id=\"" . $currentRecord["COLUMN_NAME"] . "\" value=\"\" placeholder=\"testo di max " . $maxLenght . " caratteri\">" . ($maxLenght > 20 ? "</textarea>" : "</input>");
+                        break;
+                    case "date":
+                        echo "<input type=\"date\" name=\"" . $currentRecord["COLUMN_NAME"] . "\" id=\"" . $currentRecord["COLUMN_NAME"] . "\"></input>";
+                        break;
+                    case "customText":
+                        echo "<input type=\"text\" maxlength=\"" . $configInfo[$currentRecord["COLUMN_NAME"] . 'Length'] . "\" name=\"" . $currentRecord["COLUMN_NAME"] . "\" id=\"" . $currentRecord["COLUMN_NAME"] . "\" placeholder=\"stringa di max " . $configInfo[$currentRecord["COLUMN_NAME"] . 'Length'] . " caratteri\"></input>";
+                        break;
+                }
+                echo "</td></tr>";
+            }
+        }
+        ?>
+        <tr>
+            <td>Submit</td>
+            <td><input type="submit" name="submit" value="Invia" /></td>
+        </tr>
+    </table>
+</form>
