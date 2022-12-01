239935f92ab765dc79fc42b290c7af3e6b5057ac
Joel bosio <bosio.joel@volta.ts.it>
2022-12-01 13:05:42 +0100

    partial sidebar

--- src/main/main.php
+++ src/main/main.php
@@ -1,34 +1,46 @@
-<a href="../insert/insert.php">Insert</a><br></br>
+<head>
+        <link rel="stylesheet" href="css/main.css">
+</head>
+<div class="sidebar" style="width:25%; float: left">
+        <div class="sidebarButtonCurrent"><a href="#">View</a></div>
+        <div class="sidebarButton"><a href="../insert/insert.php">Insert</a></div>
+        <div class="sidebarButton"><a href="#">Link 2</a></div>
+        <div class="sidebarButton"><a href="#">Link 3</a></div>
+</div>
 
-<?php
-require_once("../common/php/DBConnector.php");
+<div style="margin-left:25%; float: right">
 
-$connMySQL = new ConnectionMySQL();
-$pdo = $connMySQL->getConnection();
-$table = $_SESSION['table_name'];
-$maxReads = $_SESSION['rowsPerPage'];
-$page = isset($_REQUEST['page']) ? $_REQUEST['page'] : 0;
-$stmt = $pdo->prepare("SELECT * FROM $table LIMIT $maxReads OFFSET " . ($page != 0 ? (($page * $maxReads) - 1) : 0));
-$stmt->execute();
-$stmtResponse = $stmt->fetchAll();
-$stmt = $pdo->prepare("SELECT * FROM $table WHERE 1");
-$stmt->execute();
-$maxPages = ceil(count($stmt->fetchAll()) / $maxReads);
+        <?php
+        require_once("../common/php/DBConnector.php");
 
-echo '  <table>
+        $connMySQL = new ConnectionMySQL();
+        $pdo = $connMySQL->getConnection();
+        $table = $_SESSION['table_name'];
+        $maxReads = $_SESSION['rowsPerPage'];
+        $page = isset($_REQUEST['page']) ? $_REQUEST['page'] : 0;
+        $stmt = $pdo->prepare("SELECT * FROM $table LIMIT $maxReads OFFSET " . ($page != 0 ? (($page * $maxReads) - 1) : 0));
+        $stmt->execute();
+        $stmtResponse = $stmt->fetchAll();
+        $stmt = $pdo->prepare("SELECT * FROM $table WHERE 1");
+        $stmt->execute();
+        $maxPages = ceil(count($stmt->fetchAll()) / $maxReads);
+
+        echo '  <table>
         <tr><th>Id</th>     <th>Testo</th>
         </tr>';
-foreach ($stmtResponse as $currentRecord) {
-        echo    '<tr>   <td>' . $currentRecord['id'] . "</td>
+        foreach ($stmtResponse as $currentRecord) {
+                echo    '<tr>   <td>' . $currentRecord['id'] . "</td>
                     <td>" . $currentRecord['testo'] . "</td>
                     <td><a href=\"#\">Modfy</a></td>
                     <td><a href=\"#\">Delete</a></td>
             </tr>";
-}
-echo '</table>';
-?>
+        }
+        echo '</table>';
+        ?>
 
-<div id="pageWrapper">
-        <a href="main.php?page=<?php echo ($page - 1) ?>" id="previousPage" style="visibility:  <?php echo ($page <= 0 ? 'hidden;' : 'visible;') ?>">Previous</a>
-        <a href="main.php?page=<?php echo ($page + 1) ?>" id="nextPage" style="visibility:  <?php echo ($page >= ($maxPages - 1) ? 'hidden;' : 'visible;') ?>">Next</a>
+        <div id="pageWrapper">
+                <a href="main.php?page=<?php echo ($page - 1) ?>" id="previousPage" style="visibility:  <?php echo ($page <= 0 ? 'hidden;' : 'visible;') ?>">Previous</a>
+                <a href="main.php?page=<?php echo ($page + 1) ?>" id="nextPage" style="visibility:  <?php echo ($page >= ($maxPages - 1) ? 'hidden;' : 'visible;') ?>">Next</a>
+        </div>
+
 </div>
